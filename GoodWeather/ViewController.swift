8a326fad5e6cb75f2c2785a6d41eb30b6623f791
Adam Vician <adamvician@gmail.com>
2022-06-02 16:11:26 +0200

    Initial Commit

---
+++ GoodWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by Adam Vician on 02/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
